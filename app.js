65dbd6906fe6eca69fe8bb36e9ba8b819fe82f30
Said Ali <saidibnali19@gmail.com>
2023-10-14 17:18:42 +0300

    feat: learnt about route parameters

--- app.js
+++ app.js
@@ -36,8 +36,24 @@ app.all("/secret", (req, res) => {
 //     res.send("Response for any path that has letter 'a'")
 // })
 
-app.get(/.*fly$/, (req, res) => {
-    res.send("Response for /.*fly$");
+// app.get(/.*fly$/, (req, res) => {
+//     res.send("Response for /.*fly$");
+// });
+
+app.get("/users/:userId/books/:bookId", (req, res) => {
+    res.send(req.params);
+});
+
+app.get("/flights/:from-:to", (req, res) => {
+    res.send(req.params);
+});
+
+app.get("/organism/:genus.:species", (req, res) => {
+    res.send(req.params);
+});
+
+app.get("/user/:userId(\\d+)", (req, res) => {
+    res.send(req.params);
 });
 
 app.listen(PORT, () => console.log(`App running at http://localhost:${PORT}`));
